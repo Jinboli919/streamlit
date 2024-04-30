2c5729fd72948a7c07e0ac86063d825a75aeb0f6
Jinboli919 <125314665+Jinboli919@users.noreply.github.com>
2024-04-30 11:53:24 -0500

    Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,30 +3,23 @@ import pandas as pd
 import pickle
 import movie_recommendation_system
 
-
-
-st.set_page_config(page_title="Movie Recommendation System",layout="centered",page_icon=":film_projector:")
+st.set_page_config(page_title="Movie Recommendation System", layout="centered", page_icon=":film_projector:")
 st.title('Movie Recommendation System :film_projector:')
 st.sidebar.subheader('Movie Recommendation System :film_projector:')
 st.sidebar.subheader('Jinbo Li')
-option=st.sidebar.radio(
+option = st.sidebar.radio(
     "Find your movies",
-    ("Get Recommendations","EDA Parts", "TOP 50 MOVIES", "TOP 20 MOVIES in Popular Genres"))
+    ("Get Recommendations", "EDA Parts", "TOP 50 MOVIES", "TOP 20 MOVIES in Popular Genres"))
 st.sidebar.subheader(' :cinema: - About this Project')
-st.sidebar.info( """     
+st.sidebar.info("""     
 -   With the increasing availability of movies on various platforms, the challenge of selecting the most preferred movies for users has become a critical issue in the entertainment industry.
 -   The project aims to build a movie recommendation system to produce a ranked list of movies to provide personalized recommendations based on users’ preferences, rating history and other relevant factors. """)
 
 
-
 if option == "Get Recommendations":
-
-
     st.info('Which recommender would you like to use?👇')
 
-
-
-    left,mid, right=st.columns(3)
+    left, mid, right = st.columns(3)
 
     m = st.markdown("""
         <style>
@@ -42,9 +35,8 @@ if option == "Get Recommendations":
 
     with left:
         with st.expander("Content-Based"):
-
             movie_title = st.text_input("Please input movie title")
-            numbers_of_return = st.slider('Numbers of Recommendations',5,30,5,5, key = 10)
+            numbers_of_return = st.slider('Numbers of Recommendations', 5, 30, 5, 5, key=10)
             st.write("\n")
             st.write("\n")
             st.write("\n")
@@ -52,28 +44,23 @@ if option == "Get Recommendations":
             st.write("\n")
             get_recommendations = st.button('Get Recommendations-1 :tv:')
 
-
-            def content(movie_title,n):
-                recommend = movie_recommendation_system.content_recommendations_improved(movie_title,n)
-                return recommend
-
+            def content(movie_title, n):
+                try:
+                    recommend = movie_recommendation_system.content_recommendations_improved(movie_title, n)
+                    return recommend
+                except ValueError:
+                    st.warning('The movie you entered is not found in our database. Please make sure you have entered the correct movie title (including letter case).')
 
             if get_recommendations:
                 if movie_title:
-                    recommendations = content(movie_title, n= numbers_of_return)[:numbers_of_return]
+                    recommendations = content(movie_title, n=numbers_of_return)[:numbers_of_return]
                     st.write(pd.DataFrame(recommendations))
-
                 else:
                     st.warning('Please enter a movie title.')
 
-
-
-
     with mid:
         with st.expander("Collaborative Filtering"):
-
-
-            user_id = st.number_input("Please input user id",step=1)
+            user_id = st.number_input("Please input user id", step=1)
             numbers_of_return = st.slider('Numbers of Recommendations', 5, 30, 5, 5, key=11)
             st.write("\n")
             st.write("\n")
@@ -83,42 +70,39 @@ if option == "Get Recommendations":
             get_recommendations2 = st.button('Get Recommendations-2 :tv:')
 
             def cf(user_id, k=5, n=numbers_of_return):
-                recommend = movie_recommendation_system.get_recommendations_item(user_id, n=numbers_of_return)
-                return recommend
-
+                try:
+                    recommend = movie_recommendation_system.get_recommendations_item(user_id, n=numbers_of_return)
+                    return recommend
+                except ValueError:
+                    st.warning('The movie you entered is not found in our database. Please make sure you have entered the correct movie title (including letter case).')
 
             if get_recommendations2:
                 if user_id:
                     recommendations2 = cf(user_id, k=5, n=numbers_of_return)[:numbers_of_return]
                     st.write(pd.DataFrame(recommendations2))
-
                 else:
                     st.warning('Please enter a user id.')
 
-
-
     with right:
         with st.expander("Hybrid"):
-
             user_id2 = st.text_input("User id")
             movie_title2 = st.text_input("Movie title")
             numbers_of_return = st.slider('Numbers of Recommendations', 5, 30, 5, 5, key=12)
             get_recommendations3 = st.button('Get Recommendations-3 :tv:')
 
             def hy(user_id2, movie_title2, n=numbers_of_return):
-                recommend = movie_recommendation_system.hybrid(user_id2, movie_title2, n=numbers_of_return)
-                return recommend
-
+                try:
+                    recommend = movie_recommendation_system.hybrid(user_id2, movie_title2, n=numbers_of_return)
+                    return recommend
+                except ValueError:
+                    st.warning('The movie you entered is not found in our database. Please make sure you have entered the correct movie title (including letter case).')
 
             if get_recommendations3:
                 if user_id2 and movie_title2:
                     recommendations3 = hy(user_id2, movie_title2, n=numbers_of_return)[:numbers_of_return]
                     st.write(pd.DataFrame(recommendations3))
-
                 else:
                     st.warning('Please enter a user id and a movie title.')
-
-
 
 if option == "EDA Parts":
     from PIL import Image
@@ -146,13 +130,10 @@ if option == "EDA Parts":
     image8 = Image.open('image8.png')
     st.image(image8)
 
-
 if option == "TOP 50 MOVIES":
     st.subheader('TOP 50 MOVIES')
     TOP_50 = movie_recommendation_system.Top_movies
     st.write(pd.DataFrame(TOP_50))
-
-
 
 if option == "TOP 20 MOVIES in Popular Genres":
     m = st.markdown("""
@@ -169,7 +150,7 @@ if option == "TOP 20 MOVIES in Popular Genres":
 
     Genres = st.selectbox(
         'Genres',
-        ('Please select a genre', 'Action','Adventure', 'Animation', 'Comedy', 'Crime', 'Drama', 'Fantasy',
+        ('Please select a genre', 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Drama', 'Fantasy',
          'Family', 'History', 'Horror', 'Mystery', 'Romance', 'Science Fiction',
          'Thriller', 'War'))
 
@@ -180,16 +161,16 @@ if option == "TOP 20 MOVIES in Popular Genres":
 
     data = {'Genres': Genres}
 
-
-    def top20(genres, percentile=0.8, genre_name = Genres):
-        top = movie_recommendation_system.build_top(genres, percentile=0.8, genre_name = Genres)
-        return top
-
+    def top20(genres, percentile=0.8, genre_name=Genres):
+        try:
+            top = movie_recommendation_system.build_top(genres, percentile=0.8, genre_name=Genres)
+            return top
+        except ValueError:
+            st.warning('The genre you selected does not have enough movies in our database. Please try another genre.')
 
     if get_top_20:
         if Genres:
-            top_20 = top20(Genres, percentile=0.8, genre_name = Genres)
+            top_20 = top20(Genres, percentile=0.8, genre_name=Genres)
             st.write(pd.DataFrame(top_20))
-
         else:
             st.warning('Please select a genre.')
